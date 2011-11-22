fc07d1a7fc516aadcb6199ea8d3e3b28f07c466b
krongk <kenrome@gmail.com>
2011-11-22 23:55:09 +0800

    add resource upload

--- app/models/resource_item.rb
+++ app/models/resource_item.rb
@@ -1,4 +1,8 @@
 class ResourceItem < ActiveRecord::Base
   belongs_to :resource_cate
   attr_accessor :upload_file
+
+  def self.type_hash
+  	{1 => 'picture', 2 => 'file', 3 => 'video', 4 => 'audio', 5 => 'flash'}
+  end
 end
