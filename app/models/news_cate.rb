f483c28accccc16737be839fe5286840b8acf501
krongk <kenrome@gmail.com>
2013-07-06 23:13:34 +0800

    update cache

--- app/models/news_cate.rb
+++ app/models/news_cate.rb
@@ -2,7 +2,7 @@ class NewsCate < ActiveRecord::Base
   has_many :news_items
 
   before_save :translate_en
-  after_save :expire_cache
+  after_save :expire_cate_cache
 
   def translate_en
     self.en_name = Pinyin.t self.name
