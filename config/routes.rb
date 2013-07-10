0aba787b2bbdb30430d48076d3c82b677cfa59d5
krongk <kenrome@gmail.com>
2013-07-10 09:13:30 +0800

    add routs

--- config/routes.rb
+++ config/routes.rb
@@ -38,6 +38,7 @@ RailsOnWeb::Application.routes.draw do
   get "home/site_map"
   get "home/get_cdhrss"
   get "home/start_sidekiq_cd12333"
+  get "home/start_sidekiq_sina_news"
   match "form" => "home#form"
   match "search" => "home#search"
   
