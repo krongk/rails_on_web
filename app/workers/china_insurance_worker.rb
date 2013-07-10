ac297e78976e559866b1c59a9a3baf7dd5b43839
krongk <kenrome@gmail.com>
2013-07-10 09:45:48 +0800

    add china ins

--- app/workers/china_insurance_worker.rb
+++ app/workers/china_insurance_worker.rb
@@ -20,36 +20,37 @@ class ChinaInsuranceWorker
   def forage_news(list_url)
 		page = Nokogiri::HTML(open(list_url))
 		trs = page.css("body table table table table table tr")
-		return if trs.empty? 
+		return if trs.empty?
 		trs.each do |tr|
-		#get detail url
-		return if tr.css("td a").empty?
-		url = tr.css("td a")[0]['href']
-		return unless url =~ /newslist.asp\?id=\d+/
-		#http://www.china-insurance.com/news-center/newslist.asp?id=229065
-		detail_url = ("http://www.china-insurance.com/news-center/" + url).to_s
-		return if detail_url.nil?
+			#get detail url
+			next if tr.css("td a").empty?
+			url = tr.css("td a")[0]['href']
+			next unless url =~ /newslist.asp\?id=\d+/
+			#http://www.china-insurance.com/news-center/newslist.asp?id=229065
+			detail_url = ("http://www.china-insurance.com/news-center/" + url).to_s
+			next if detail_url.nil?
 
-		#check dup
-		return if NewsItem.find_by_original_url(detail_url)
-		detail_page = Nokogiri::HTML(open(detail_url))
-		
-		title = detail_page.css("td h1").inner_text
-		title = detail_page.css("p.f20b").inner_text if title.blank?
-		body = detail_page.css("body table table table table").inner_html
-		return if title.blank? || body.blank?
+			#check dup
+			next if NewsItem.find_by_original_url(detail_url)
+			detail_page = Nokogiri::HTML(open(detail_url))
+			
+			title = detail_page.css("td h1").inner_text
+			title = detail_page.css("p.f20b").inner_text if title.blank?
+			body = detail_page.css("body table table table table").inner_html
+			next if title.blank? || body.blank?
 
-		#check dup of news
-		return if NewsItem.find_by_title(title)
+			#check dup of news
+			next if NewsItem.find_by_title(title)
 
-		@news_cate.news_items.create(
-			:original_url => detail_url,
-			:title => title,
-			:body => body,
-			:meta_keywords => "#{title} 成都社保局 成都保险咨询网",
-			:meta_description => title
-		)
-		puts "done tr: #{detail_url}"
+			@news_cate.news_items.create(
+				:original_url => detail_url,
+				:title => title,
+				:body => body,
+				:meta_keywords => "#{title} 成都社保局 成都保险咨询网",
+				:meta_description => title
+			)
+			puts "done tr: #{detail_url}"
+		end
   end
 end
 
